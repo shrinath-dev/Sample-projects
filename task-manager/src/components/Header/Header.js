372858109571c678cfe5cb4ffd6bb85b125fc7df
shrinath-dev <shrinath200802@gmail.com>
2025-12-11 14:38:38 +0530

    remove dependencies

--- task-manager/src/components/Header/Header.js
+++ task-manager/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import styles from './Header.module.css';
 import { useThemeContext } from "../../contexts/ThemeContext/ThemeContext";
 
