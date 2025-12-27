64a6efe10a4dba2a8a75a6a228fb255072541a8e
shrinath-dev <shrinath200802@gmail.com>
2025-12-27 23:15:00 +0530

    finalize search functionality

--- phlipkart/src/components/ProductGrid/ProductGrid.js
+++ phlipkart/src/components/ProductGrid/ProductGrid.js
@@ -3,6 +3,7 @@ import styles from './ProductGrid.module.css';
 import { fetchProducts, productSearchResults } from "../../features/products/productSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { ProductCard } from "../subComponents";
+import { getSearchTerm, getCategory } from "../../features/filters/filterSlice";
 
 function ProductGrid() {
     const dispatch = useDispatch();
@@ -12,34 +13,54 @@ function ProductGrid() {
 
     const products = useSelector(productSearchResults);
     const fetchStatus = useSelector(state => state.products.status);
-    console.log(fetchStatus);
+    const category = useSelector(getCategory);
+    const searchTerm = useSelector(getSearchTerm);
 
     return (
         <div className={styles.productsContainer}>
-            <h2 className={styles.heading}>All Products</h2>
+            <h2 className={styles.heading}>
+                {
+                    searchTerm === '' ? (
+                        'All Products ' + (category !== 'all' ? `in "${category}"` : '')
+                    ) : (
+                        `(${products.length}) Search Results For "${searchTerm}" ` + (category !== 'all' ? `in "${category}"` : '')
+                    )
+                }
+            </h2>
             {
-                fetchStatus === 'loading' ? (
-
-                    <div className={styles.loaderContainer}>
-                        <div className={styles.loader}></div>
-                        <p className={styles.loadingText}>Loading Products...</p>
+                products.length === 0 ? (
+                    <div className={styles.empty}>
+                        {
+                            searchTerm === '' ? (
+                                'No products founds ' + (category !== 'all' ? `in "${category}"` : '')
+                            ) : (
+                                `No products found for "${searchTerm}" ` + (category !== 'all' ? `in "${category}"` : '')
+                            )
+                        }
                     </div>
-                ) : (
-                    fetchStatus === 'failed' ? (
-                        <div className={styles.fetchError}>
-                            <p className={styles.errorMsg}>Failed to load product, please try again</p>
+                ) :
+                    (fetchStatus === 'loading' ? (
+
+                        <div className={styles.loaderContainer}>
+                            <div className={styles.loader}></div>
+                            <p className={styles.loadingText}>Loading Products...</p>
                         </div>
                     ) : (
-                        <div className={styles.productGrid}>
-                            {
-                                products.map(product => (
-                                    <ProductCard key={product.id} product={product} />
-                                ))
-                            }
-                        </div>
-                    )
+                        fetchStatus === 'failed' ? (
+                            <div className={styles.fetchError}>
+                                <p className={styles.errorMsg}>Failed to load product, please try again</p>
+                            </div>
+                        ) : (
+                            <div className={styles.productGrid}>
+                                {
+                                    products.map(product => (
+                                        <ProductCard key={product.id} product={product} />
+                                    ))
+                                }
+                            </div>
+                        )
 
-                )
+                    ))
             }
         </div>
     )
