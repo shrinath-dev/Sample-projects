226ca17d57bf87c9ea4fdbf0df077172e46a47d7
shrinath-dev <shrinath200802@gmail.com>
2025-12-29 17:07:30 +0530

    working on login form

--- phlipkart/src/components/LoginForm/LoginForm.js
+++ phlipkart/src/components/LoginForm/LoginForm.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import styles from './LoginForm.module.css';
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { BiSolidHide } from "react-icons/bi";
+import { BiSolidShow } from "react-icons/bi";
+
 
 
 function LoginForm() {
@@ -7,6 +12,8 @@ function LoginForm() {
         username: '',
         password: '',
     })
+
+    const [showPassword, setShowPassword] = useState(false);
 
 
     const handleChange = (name, value) => {
@@ -16,23 +23,34 @@ function LoginForm() {
         })
     }
 
+
     const handleSubmit = () => {
         // later 
     }
 
     return (
         <div className={styles.formContainer}>
-            <form name="login" onSubmit={handleSubmit}>
+            <form name="login" className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.formGroup}>
-                    <label for='username' className={styles.label}>Username</label>
-                    <input type="text" name='username' value={formData.username}
-                        onChange={(e) => handleChange(e.target.name, e.target.value)} />
+                    <FaUser className={styles.icon} />
+                    <label htmlFor='username' className={styles.label}>Username</label>
+                    <input autoComplete="true" name="username" id="username" type="text" value={formData.username}
+                        onChange={(e) => handleChange(e.target.name, e.target.value)}
+                        required />
                 </div>
 
                 <div className={styles.formGroup}>
-                    <label for='password' className={styles.label}>Password</label>
-                    <input type="password" name='password' value={formData.password}
-                        onChange={(e) => handleChange(e.target.name, e.target.value)} />
+                    <RiLockPasswordFill className={styles.icon} />
+                    <label htmlFor='password' className={styles.label}>Password</label>
+                    <input name="password" id="password" type={showPassword ? 'text' : 'password'} value={formData.password}
+                        onChange={(e) => handleChange(e.target.name, e.target.value)}
+                        required />
+                    <div onClick={() => setShowPassword(prev => !prev)} className={styles.passVisibilityToggler}>
+                        {
+                            showPassword ? <BiSolidShow className={styles.icon} /> : <BiSolidHide className={styles.icon} />
+                        }
+                    </div>
+
                 </div>
 
                 <button type='submit'>Submit</button>
--- phlipkart/src/pages/LoginPage/LoginPage.js
+++ phlipkart/src/pages/LoginPage/LoginPage.js
@@ -1,13 +1,23 @@
 import React from "react";
 import styles from './LoginPage.module.css';
 import { LoginForm } from "../../components";
+import { useThemeContext } from "../../context";
 
 
 function LoginPage() {
 
+    const { theme } = useThemeContext();
+
     return (
-        <div className={styles.loginPageContainer}>
-            <LoginForm />
+        <div data-theme={theme} className={styles.loginPageContainer}>
+
+            <div className={styles.loginForm}>
+                <div className={styles.sectionHead}>
+                    <p className={styles.heading}>Welcome Back</p>
+                    <p className={styles.subHeading}>Please Enter Your Account Details.</p>
+                </div>
+                <LoginForm />
+            </div>
         </div>
     )
 };
