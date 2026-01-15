08f92d9db29e2160ee5354de824d341856aecbb3
shrinath-dev <shrinath200802@gmail.com>
2026-01-15 11:01:38 +0530

    fix broken links on landing page

--- book-worm/app/staticComponents/Footer/Footer.tsx
+++ book-worm/app/staticComponents/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer(){
     const year = new Date().getFullYear();
     return(
-        <footer className='flex justify-center items-center py-5 text-muted-foreground '>
-            <p>&copy; {year} BookWorm. All rights reserved. </p>
+        <footer className='flex justify-center items-center py-20 text-muted-foreground '>
+            <p className='text-sm'>&copy; {year} BookWorm. All rights reserved. Made with ❤️ for readers everywhere.</p>
         </footer>
     )
 }
